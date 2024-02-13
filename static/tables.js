ec36a2399851486dd0616a3193a65fbce35b718d
Nicholas Michaud <nmichaud@google.com>
2024-02-12 20:18:29 -0500

    message

--- static/tables.js
+++ static/tables.js
@@ -1,5 +1,14 @@
-import { db } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
  
-const client = await db.connect();
-await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-   
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  try {
+    const result =
+      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}
